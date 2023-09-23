83274ac2ee00cf6d72d9c95c896917acd5b536a6
yuvrajkaushal9 <119110977+yuvrajkaushal9@users.noreply.github.com>
2023-09-23 14:39:44 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university.management.system;
+
+import java.sql.*;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+
+    Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem", "root", "hathiyar10");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
